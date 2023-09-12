67e8f2586ac849a02da6b7f35af188b7161f5bd0
Li, Zhongheng <zhongheng.li@str.us>
2023-09-11 23:18:38 -0400

    Train 1v1 with pretained blue

--- multigrid/scripts/visualize.py
+++ multigrid/scripts/visualize.py
@@ -145,6 +145,7 @@ def main_evaluation(args):
         num_workers=0,
         num_gpus=0,
     )
+    config.explore = False
     config.environment(disable_env_checking=True)
     algorithm = config.build()
     checkpoint = get_checkpoint_dir(args.load_dir)
